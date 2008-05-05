2cdaacf95e196865af6cf77a3b54f65528c8302d
bpfurtado <bpfurtado@94baabd9-194b-0410-a295-71277146d4dd>
2008-05-05 02:49:34 +0000

    added tabs with system information

--- trunk/src/main/java/net/bpfurtado/tas/view/AboutFrame.java
+++ trunk/src/main/java/net/bpfurtado/tas/view/AboutFrame.java
@@ -20,14 +20,16 @@
  */
 package net.bpfurtado.tas.view;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.beans.PropertyChangeListener;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
-import javax.swing.Action;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -36,6 +38,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
@@ -56,8 +59,7 @@ public class AboutFrame extends JDialog
 		widgets();
 
 		setTitle("About - Text Adventures Suite");
-		
-		Util.centerPosition(invokerFrame, this, 500, 320);
+		Util.centerPosition(invokerFrame, this, 516, 339);
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setDefaultLookAndFeelDecorated(true);
@@ -70,6 +72,7 @@ public class AboutFrame extends JDialog
 	{
 		JPanel mainPn = new JPanel();
 		mainPn.setLayout(new BoxLayout(mainPn, BoxLayout.PAGE_AXIS));
+
 		mainPn.add(Box.createRigidArea(new Dimension(0, 10)));
 
 		JLabel title = new JLabel("Text Adventures Suite");
@@ -78,27 +81,70 @@ public class AboutFrame extends JDialog
 		mainPn.add(title);
 
 		mainPn.add(Box.createRigidArea(new Dimension(0, 10)));
-
-		JTextArea ta = new JTextArea(
-				"Text Adventures Suite is a authoring and interpreter of Text Adventures largely inspired on \n" + 
-				"Steve Jackson and Ian Livingstone Fighting Fantasy series of game books in the 80s.\n" + 
-				"\n" + 
-				"It has been more than a pleasure to me to create one step at a time this software. \n" + 
-				"It\'s distributed totally for free, its source code will be released more likely after \n" + 
-				"the 1.0 version.\n" + "\n" + "More about me on my Software Development Blog.\n" + 
-				"http://bpfurtado.livejournal.com\n" +
-				"\n" + 
-				"Special thanks to my wife Andrea for being so important in my life.\n" +
-				"\n" + 
-				"Visit: http://bpfurtado.net/tas");
-
-		ta.setEditable(false);
-		ta.setWrapStyleWord(true);
-		mainPn.add(new JScrollPane(ta));
-
+		mainPn.add(createTabsPanel());
 		mainPn.add(Box.createRigidArea(new Dimension(0, 5)));
+		mainPn.add(createCloseBt());
+        mainPn.add(Box.createRigidArea(new Dimension(0, 5)));
 
-		JButton closeBt = new JButton("Close");
+        mainPn.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), DisposeAction.ACTION_NAME);
+        mainPn.getActionMap().put(DisposeAction.ACTION_NAME, new DisposeAction(this));
+        
+        add(mainPn);
+    }
+
+    private JTabbedPane createTabsPanel()
+    {
+        JTabbedPane tabs = new JTabbedPane();
+		tabs.addTab("About", createAboutPanel());
+		tabs.addTab("System Properties", createSystemPropertiesPanel());
+		tabs.addTab("Environment Variables", createEnvironmentVariablesPanel());
+        return tabs;
+    }
+
+    private Component createEnvironmentVariablesPanel()
+    {
+        return createTAPanel(getEnvironmentVars());
+    }
+
+    private JPanel createSystemPropertiesPanel()
+    {
+        return createTAPanel(getSystemProperties());
+    }
+
+    private JPanel createTAPanel(String text)
+    {
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.LINE_AXIS));
+        
+        JTextArea ta = new JTextArea(text);
+        ta.setEditable(false);
+        p.add(new JScrollPane(ta));
+        return p;
+    }
+
+    private String getSystemProperties()
+    {
+        Set<Entry<Object, Object>> entrySet = System.getProperties().entrySet();
+        StringBuilder b = new StringBuilder();
+        for (Entry<Object, Object> e : entrySet) {
+            b.append(e.getKey() + " = " + e.getValue() + "\n");
+        }
+        return b.toString();
+    }
+
+    private String getEnvironmentVars()
+    {
+        Map<String, String> environment = System.getenv();
+        StringBuilder b = new StringBuilder();
+        for (String var : environment.keySet()) {
+            b.append(var + " = " + environment.get(var) + "\n");
+        }
+        return b.toString();
+    }
+
+    private JButton createCloseBt()
+    {
+        JButton closeBt = new JButton("Close");
 		closeBt.setMnemonic('C');
 		closeBt.setAlignmentX(CENTER_ALIGNMENT);
 		closeBt.addActionListener(new ActionListener()
@@ -106,17 +152,35 @@ public class AboutFrame extends JDialog
 			public void actionPerformed(ActionEvent e)
 			{
 				dispose();
+				System.out.println(".actionPerformed() +" + getBounds());
             }
         });
         getRootPane().setDefaultButton(closeBt);
-        mainPn.add(closeBt);
+        return closeBt;
+    }
 
-        mainPn.add(Box.createRigidArea(new Dimension(0, 5)));
-
-        mainPn.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), DisposeAction.ACTION_NAME);
-        mainPn.getActionMap().put(DisposeAction.ACTION_NAME, new DisposeAction(this));
+    private JPanel createAboutPanel()
+    {
+        JPanel aboutPn = new JPanel();
+        aboutPn.setLayout(new BoxLayout(aboutPn, BoxLayout.LINE_AXIS));
         
-        add(mainPn);
+		JTextArea ta = new JTextArea(
+				"Text Adventures Suite is a authoring and interpreter of Text Adventures largely inspired on \n" + 
+				"Steve Jackson and Ian Livingstone Fighting Fantasy series of game books in the 80s.\n" + 
+				"\n" + 
+				"It has been more than a pleasure to me to create one step at a time this software. \n" + 
+				"It\'s free and open source (GPLv2): http://code.google.com/p/text-adventures-suite \n" + 
+				"\n" + 
+				"More about me on my Software Development Blog.\n" + 
+				"http://bpfurtado.livejournal.com\n" +
+				"\n" + 
+				"Special thanks to my wife Andrea for being so important in my life.\n" +
+				"\n" + 
+				"Visit: http://bpfurtado.net/tas");
+		ta.setEditable(false);
+		ta.setWrapStyleWord(true);
+		aboutPn.add(new JScrollPane(ta));
+        return aboutPn;
     }
 
 	public static void main(String[] args)
@@ -134,3 +198,4 @@ public class AboutFrame extends JDialog
 		});
 	}
 }
+
